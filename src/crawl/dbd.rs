191bc176ca2a23c8d616cfd66485fdc14723bfff
Konstantin Lukas <konstantin_lukas@web.de>
2024-04-06 12:53:38 +0200

    refactor crawling function

--- src/crawl/dbd.rs
+++ src/crawl/dbd.rs
@@ -8,37 +8,43 @@ use crate::utils::message::MessageHandler;
 
 
 pub async fn run(message_handler: &MessageHandler) {
-    let pcg = reqwest::get("https://www.pcgamesn.com/dead-by-daylight/dbd-codes");
-    let rps = reqwest::get("https://www.rockpapershotgun.com/dead-by-daylight-codes-list");
-    let dex = reqwest::get("https://www.dexerto.com/dead-by-daylight/how-to-redeem-dead-by-deadlight-codes-1664016/");
-
-    let results = future::join_all([pcg, rps, dex]).await;
-
-    let mut ok_results = Vec::new();
-    for result in results {
-        if let Ok(response) = result {
-            ok_results.push(response.text())
-        }
-    }
-    let results = future::join_all(ok_results).await;
 
     // MATCH COLON AND DIFFERENT TYPES OF DASHES
-    let pattern = Regex::new(
+    let pattern = match Regex::new(
         r"[\u003A\u2013\u002D\u05BE\u1806\u2010\u2015\u2E3A\u2E3B\uFE58\uFE63\uFF0D]"
-    ).unwrap();
+    ) {
+        Ok(regex) => regex,
+        Err(e) => {
+            message_handler.dm_admin(
+                format!("An error occurred trying to construct regex: ```\n{e}\n```").as_str()
+            ).await;
+            return;
+        }
+    };
+
+    let results = future::join_all([
+        reqwest::get("https://www.pcgamesn.com/dead-by-daylight/dbd-codes"),
+        reqwest::get("https://www.rockpapershotgun.com/dead-by-daylight-codes-list"),
+        reqwest::get("https://www.dexerto.com/dead-by-daylight/how-to-redeem-dead-by-deadlight-codes-1664016/")]
+    ).await;
+
+    let results = future::join_all(
+        results.into_iter().flatten().map(|x| x.text())
+    ).await.into_iter().flatten().collect::<Vec<_>>();
 
     let mut codes: HashMap<String, String> = HashMap::new();
     let mut new_codes: HashMap<String, String> = HashMap::new();
 
+    let file;
     match fs::read_to_string("dbd_codes.csv") {
-        Ok(file) => {
-            let lines = file.split('\n');
-            for line in lines {
+        Ok(data) => {
+            file = data;
+            for line in file.lines() {
                 let parts: Vec<&str> = line.split('\t').collect();
                 if parts.len() == 2 {
                     codes.insert(
-                        String::from(parts[0]),
-                        String::from(parts[1])
+                        parts[0].to_string(),
+                        parts[1].to_string()
                     );
                 }
             }
@@ -52,25 +58,25 @@ pub async fn run(message_handler: &MessageHandler) {
     }
 
     for result in results {
-        if let Ok(text) = result {
-            let document = Html::parse_document(&text);
-            let ul_selector = Selector::parse("h2 ~ ul").unwrap();
-            let li_selector = Selector::parse("li").unwrap();
-            if let Some(ul) = document.select(&ul_selector).next() {
-                for element in ul.select(&li_selector) {
-                    let text_content: String = element.text().collect();
-                    let res: Vec<String> = pattern.split(text_content.as_str()).map(|s| s.to_string()).collect();
-                    if res.len() == 2 {
-                        let key = String::from(res[0].trim());
-                        let value = String::from(res[1].trim());
-                        if !codes.contains_key(key.as_str()) {
-                            codes.insert(key.clone(), value.clone());
-                            new_codes.insert(key, value);
-                        }
+        let document = Html::parse_document(&result);
+        let ul_selector = Selector::parse("h2 ~ ul").unwrap();
+        let li_selector = Selector::parse("li").unwrap();
+        if let Some(ul) = document.select(&ul_selector).next() {
+            for element in ul.select(&li_selector) {
+                let text_content = element.text().collect::<String>();
+                let res = pattern
+                    .split(text_content.as_str())
+                    .map(|x| x.to_string())
+                    .collect::<Vec<String>>();
+                if res.len() == 2 {
+                    let key = res[0].trim().to_string();
+                    let value = res[1].trim().to_string();
+                    if !codes.contains_key(&key) {
+                        codes.insert(key.clone(), value.clone());
+                        new_codes.insert(key, value);
                     }
                 }
             }
-
         }
     }
 
@@ -80,8 +86,8 @@ pub async fn run(message_handler: &MessageHandler) {
     }
 
     if new_codes.len() > 0 {
-        let mut message = String::from("I found some fresh new codes since my last crawl! 🩸\n\n");
-        for new_code in new_codes {
+        let mut message = String::from("I found some fresh new codes since my last crawl! 🩸\n");
+        for (code, desc) in new_codes {
             let utc_now: DateTime<Utc> = Utc::now();
             let mut formatted_date = None;
             if let Some(gmt1_offset) = FixedOffset::east_opt(3600) {
@@ -90,24 +96,21 @@ pub async fn run(message_handler: &MessageHandler) {
             }
             if let Some(date) = formatted_date {
                 message.push_str(
-                    format!("- `{}`: {} ({})\n", new_code.0, new_code.1,date).as_str()
+                    format!("- `{}`: {} ({})\n", code, desc,date).as_str()
                 );
             } else {
                 message.push_str(
-                    format!("- `{}`: {}\n", new_code.0, new_code.1).as_str()
+                    format!("- `{}`: {}\n", code, desc).as_str()
                 );
             }
         }
         message_handler.post_to_dbd_channel(message.as_str()).await;
     }
 
-    match fs::write("dbd_codes.csv", file_content) {
-        Ok(_) => { },
-        Err(e) => {
-            message_handler.dm_admin(
-                format!("An error occurred trying to write DBD codes to file: ```\n{e}\n```").as_str()
-            ).await;
-        }
+    if let Err(e) = fs::write("dbd_codes.csv", file_content) {
+        message_handler.dm_admin(
+            format!("An error occurred trying to write DBD codes to file: ```\n{e}\n```").as_str()
+        ).await;
     }
 
 }
